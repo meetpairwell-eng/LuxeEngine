d8b29d9838bf64603c1380a01e643206775c7fec
Hector Meza-Zepeda <hectormeza-zepeda@Hectors-MacBook-Air.local>
2026-01-29 14:47:02 -0600

    Fix import error: Update lobello ID from 4610 to 5610 across the app

--- src/config/propertyConfig.js
+++ src/config/propertyConfig.js
@@ -1,4 +1,4 @@
-import * as lobello4610 from './properties/lobello4610';
+import * as lobello5610 from './properties/lobello5610';
 import * as template from './properties/template';
 
 import * as brickellia5168 from './properties/brickellia5168';
@@ -7,17 +7,17 @@ import * as brickellia5168 from './properties/brickellia5168';
 // import * as oaklawn from './properties/oaklawn';
 
 const properties = {
-    'lobello4610': lobello4610,
+    'lobello5610': lobello5610,
     'template': template,
 
     'brickellia5168': brickellia5168,
 };
 
 // VITE_PROPERTY_ID is set in your Dokploy Environment Variables
-// If not set, it defaults to 'lobello4610'
-const activeId = import.meta.env.VITE_PROPERTY_ID || 'lobello4610';
+// If not set, it defaults to 'lobello5610'
+const activeId = import.meta.env.VITE_PROPERTY_ID || 'lobello5610';
 
-const config = properties[activeId] || properties['lobello4610'];
+const config = properties[activeId] || properties['lobello5610'];
 
 export const { propertyInfo, agentInfo, images, galleryConfig, detailSections, theme, layout, analytics, galleryConfig_LEGACY } = config;
 export { activeId };
